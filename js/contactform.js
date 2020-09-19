e2f9f3f4392b992de37230985cf2ab49769545a7
Harry Tran <harrytr4n@gmail.com>
2020-09-18 17:30:38 -0700

    day 5 commit

---
+++ js/contactform.js
@@ -0,0 +1,43 @@
+//firebase link
+var firebaseConfig = {
+    apiKey: "AIzaSyCRYsXazlUcWDmsWOYmJWdp_2JKjinPyYQ",
+    authDomain: "contactmeform-3de70.firebaseapp.com",
+    databaseURL: "https://contactmeform-3de70.firebaseio.com",
+    projectId: "contactmeform-3de70",
+    storageBucket: "contactmeform-3de70.appspot.com",
+    messagingSenderId: "868962792991",
+    appId: "1:868962792991:web:00b5f76739b7bf303c872a",
+    measurementId: "G-P80V6E5WZQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var msgRef = firebase.database().ref('messages');
+
+document.getElementById('contactmeform').addEventListener('submit', submitForm);
+
+function submitForm(e){
+	e.preventDefault();
+	var name = getInput('name');
+	var contact = getInput('submittercontact');
+	var msg = getInput('msg');
+
+	sendMsg(name, contact, msg);
+
+	console.log(name);
+	console.log(contact);
+	console.log(msg);
+
+	document.getElementById('contactmeform').reset();
+}
+
+function getInput(id){
+	return document.getElementById(id).value;
+}
+
+function sendMsg(name, contact, msg) {
+	var newMsgRef= msgRef.push();
+	newMsgRef.set({
+		name: name, contact: contact, msg: msg
+	});
+}
